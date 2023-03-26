9c515af8546e476f467e81c272097c450c1ccd87
ddulyas <120781735+ddulyas@users.noreply.github.com>
2023-03-26 20:48:26 +0500

    add interceptor for get

--- src/http/index.ts
+++ src/http/index.ts
@@ -1,6 +1,7 @@
 import axios from "axios";
+import { AuthResponse } from "../models/response/AuthResponse"; 
 
-export const API_URL = 'http:/localhost/3000/api'
+export const API_URL = 'http://localhost:5000/api'
 
 
 const $api = axios.create({
@@ -12,6 +13,23 @@ const $api = axios.create({
 $api.interceptors.request.use((config) => {
     config.headers.Authorization = `Bearer ${localStorage.getItem('token')}`
     return config
+})
+
+$api.interceptors.response.use((config) => {
+    return config
+}, async (err) => {
+    const originalRequest = err.config
+    if (err.response.status == 401 && err.config && !err.config._isRetry) {
+        originalRequest._isRetry = true
+        try {
+            const response = await axios.get<AuthResponse>(`${API_URL}/refresh`, {withCredentials: true})
+            localStorage.setItem('token', response.data.accessToken)
+            return $api.request(originalRequest)
+        } catch (error) {
+            console.log('не авторизован')
+        }
+    }
+    throw err
 })
 
 export default $api
