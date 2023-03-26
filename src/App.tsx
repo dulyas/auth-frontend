edce0659987d733463f5b7c78a3f5d466c357e11
ddulyas <120781735+ddulyas@users.noreply.github.com>
2023-03-26 20:48:34 +0500

    add auth logic

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,72 @@
-import { useState } from 'react'
-
+import { useContext, useEffect, useState } from 'react'
+import LoginForm from './components/LoginForm'
+import { Context } from './main'
+import { observer } from 'mobx-react-lite'
+import { IUser } from './models/IUser'
+import UserService from './service/UserService'
 
 function App() {
-  const [count, setCount] = useState(0)
+
+
+  const {store} = useContext(Context)
+  const [users, setUsers] = useState<IUser[]>([])
+
+  useEffect(() => {
+    if (localStorage.getItem('token')) {
+      store.checkAuth()
+    }
+  }, [])
+
+  async function getUsers() {
+    try {
+      const response = await UserService.fetchUsers()
+      setUsers(response.data)
+    } catch (error) {
+      console.log(error)
+    }
+  }
+
+  if (store.isLoading) {
+    return (
+      <div>
+        Загрузка
+      </div>
+    )
+  }
+
+  if (!store.isAuth) {
+    return (
+      <>
+        <h1>
+          Пользователь не авторизован
+        </h1>
+        <LoginForm />
+      </>
+    )
+  }
 
   return (
     <>
-    
+      <h1>
+        {`Пользователь ${store.user.email} авторизован`}
+      </h1>
+      <h2>
+        {store.user.isActivated ? 'Аккаунт активирован' : 'Аккаунт не активирован'}
+      </h2>
+      <button
+        onClick={() => store.logout()}> 
+        Выйти
+      </button>
+      <div>
+        <button onClick={getUsers}>
+          Получить пользователей
+        </button>
+        {users.map(user => 
+          <div key={user.email}>{user.email}</div>
+          )}
+      </div>
     </>
   )
 }
 
-export default App
+export default observer(App)
