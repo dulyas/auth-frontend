c52fc0dc7e23f4f6bdbd3766ad52cfbfc20b3fb9
ddulyas <120781735+ddulyas@users.noreply.github.com>
2023-03-26 20:47:52 +0500

    add mobx store

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,25 @@
-import React from 'react'
+import React, { createContext } from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
 import './index.css'
+import Store from './store/store'
+
+const store = new Store()
+
+interface State {
+  store: Store
+}
+
+export const Context = createContext<State>({
+  store
+})
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  // <React.StrictMode>
+    <Context.Provider value={{
+      store
+    }}>
+        <App />
+    </Context.Provider>
+  // </React.StrictMode>,
 )
--- src/store/store.ts
+++ src/store/store.ts
@@ -1,5 +1,78 @@
+import { API_URL } from './../http/index';
+import { AuthResponse } from './../models/response/AuthResponse';
+import axios from "axios";
+import { IUser } from "../models/IUser";
+import { makeAutoObservable } from "mobx";
+import AuthService from "../service/AuthService";
+
 export default class Store {
+    user = {} as IUser
+    isAuth = false
+    isLoading = false
+
     constructor() {
-        
+        makeAutoObservable(this)
     }
+
+    setLoading(status: boolean) {
+        this.isLoading = status
+    }
+
+    setAuth(status: boolean) {
+        this.isAuth = status
+    }
+
+    setUser(user: IUser) {
+        this.user = user
+    }
+
+    async login(email: string, password: string) {
+        try {
+            const response = await AuthService.login(email, password)
+            console.log(response)
+            localStorage.setItem('token', response.data.accessToken)
+            this.setAuth(true)
+            this.setUser(response.data.user)
+        } catch (error) {
+            console.log(error.response?.data?.message || error)
+        }
+    }
+
+    async registration(email: string, password: string) {
+        try {
+            const response = await AuthService.registration(email, password)
+            console.log(response)
+            localStorage.setItem('token', response.data.accessToken)
+            this.setAuth(true)
+            this.setUser(response.data.user)
+        } catch (error) {
+            console.log(error.response?.data?.message || error)
+        }
+    }
+
+    async logout() {
+        try {
+            const response = await AuthService.logout
+            localStorage.removeItem('token')
+            this.setAuth(false)
+            this.setUser({} as IUser)
+        } catch (error) {
+            console.log(error.response?.data?.message || error)
+        }
+    }
+
+    async checkAuth() {
+        try {
+            this.setLoading(true)
+            const response = await axios.get<AuthResponse>(`${API_URL}/refresh`, {withCredentials: true})
+            localStorage.setItem('token', response.data.accessToken)
+            this.setAuth(true)
+            this.setUser(response.data.user)
+        } catch (error) {
+            console.log(error.response?.data?.message || error)
+        } finally {
+            this.setLoading(false)
+        }
+    }
+
 }
